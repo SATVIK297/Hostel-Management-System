9b3dbcfec164f8720706da884dd67625954fb1b6
satvik297 <satvikkakkar5@gmail.com>
2024-08-11 17:44:50 +0530

    sync changes

--- Frontend/src/admin_pages/Roomclean_requests.jsx
+++ Frontend/src/admin_pages/Roomclean_requests.jsx
@@ -89,7 +89,7 @@
 // export default Roomcleaning_requests;
 
 
-import React, { useState, useEffect } from 'react';
+import  { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useSelector } from 'react-redux';
 
